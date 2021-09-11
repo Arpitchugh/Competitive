50a258fda728291c035a761be69f9ed3f8ae3c4b
ArpitChugh <ArpitChugh.jbd.ac@gmail.com>
2021-09-11 16:07:19 +0530

    primeSum question

--- apnaCollege/array/pairSum.cpp
+++ apnaCollege/array/pairSum.cpp
@@ -6,12 +6,12 @@ bool pairSum(int arr[], int n, int num)
 {
     int low = 0;
     int high = n - 1;
-    
+
     while (low < high)
     {
         if (arr[low] + arr[high] == num)
         {
-            cout << low << " " << high<<endl;
+            cout << low << " " << high << endl;
             return true;
         }
         else if (arr[low] + arr[high] < num)
@@ -29,15 +29,15 @@ bool pairSum(int arr[], int n, int num)
 int main()
 {
     //Input Array
-    int n;
-    cin >> n;
-    int arr[n];
+    int n=6;
+    // cin >> n;
+    int arr[] = {1,4,6,2,7,9};
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
     int num;
     cin >> num;
-    pairSum(arr, n, num);
+    cout << pairSum(arr, 6, num);
     return 0;
 }
