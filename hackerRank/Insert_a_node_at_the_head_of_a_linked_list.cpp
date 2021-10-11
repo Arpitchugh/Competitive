1d263c27d111ef0700a532c88e0affee1553c097
ArpitChugh <ArpitChugh.jbd.ac@gmail.com>
2021-10-11 12:39:29 +0530

    insertion at a particular place in linkedList

--- hackerRank/Insert_a_node_at_the_head_of_a_linked_list.cpp
+++ hackerRank/Insert_a_node_at_the_head_of_a_linked_list.cpp
@@ -67,7 +67,48 @@ void free_singly_linked_list(SinglyLinkedListNode *node)
  */
 SinglyLinkedListNode *insertNodeAtHead(SinglyLinkedListNode *llist, int data)
 {
-    
+    SinglyLinkedListNode *temp = (SinglyLinkedListNode *)(malloc(sizeof(SinglyLinkedListNode)));
+    temp->data = data;
+    temp->next = NULL;
+    if (llist == NULL)
+    {
+        llist = temp;
+    }
+    else
+    {
+        temp->next = llist;
+        llist = temp;
+    }
+
+    return llist;
 }
 
 int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    SinglyLinkedList *llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++)
+    {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        SinglyLinkedListNode *llist_head = insertNodeAtHead(llist->head, llist_item);
+        llist->head = llist_head;
+    }
+
+    print_singly_linked_list(llist->head, "\n", fout);
+    fout << "\n";
+
+    free_singly_linked_list(llist->head);
+
+    fout.close();
+
+    return 0;
+}
