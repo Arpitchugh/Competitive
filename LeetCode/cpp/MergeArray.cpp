48516b271ed46bc2b71392c747bbb36754c3df41
ArpitChugh <arpitchugh.jbd.ac@gmail.com>
2022-03-06 11:13:20 +0530

    Merge Sorted Arrays optimized Solution

--- LeetCode/cpp/MergeArray.cpp
+++ LeetCode/cpp/MergeArray.cpp
@@ -2,10 +2,25 @@
 using namespace std;
 void merge(vector <int> &a , int m , vector <int> &b , int n)
 {
-    for(int i = 0 ; i < n ; i++)
-        a[i + m] = b[i];
-    sort(a.begin() , a.end());
-    return;
+// Brute Force
+    // for(int i = 0 ; i < n ; i++)
+    //     a[i + m] = b[i];
+    // sort(a.begin() , a.end());
+
+// Optimized
+    int i=nums1.size()-1,j=m-1,k=n-1;
+
+        while(j>=0 && k>=0){
+            if(nums1[j] > nums2[k]){
+                nums1[i--] = nums1[j--];
+            }else{
+                nums1[i--] = nums2[k--];
+            }
+        }
+        while(k>=0){
+            nums1[i--] = nums2[k--];
+        }
+    }
 }
 int main()
 {
