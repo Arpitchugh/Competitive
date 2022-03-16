f11ec8fed3e2532e6adb17f4e7aafd17e122dcb4
ArpitChugh <arpitchugh.jbd.ac@gmail.com>
2022-03-16 12:33:23 +0530

    code to find lexographic strings

---
+++ LeetCode/cpp/minimizedMaximumNumber.cpp
@@ -0,0 +1,22 @@
+// Coding Ninja Code Kaze Question
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int ans=0;
+  cin >> ans;
+  int val[10];
+  std::cin >> /* variable */;
+  std::vector<int> a;
+  for (size_t i = 0; i < 10; i++) {
+    cin >> val;
+    a.push_back(val);
+  }
+  while(k>0){
+    auto ans = *max_element(a.begin(),a.end());
+    ans = max/2;
+    k--;
+  }
+  std::cout << ans << '\n';
+}
