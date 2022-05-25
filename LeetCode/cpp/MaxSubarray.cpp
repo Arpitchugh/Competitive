b3d567dead4d5a54ca92db8dbb30aa66f1d025a7
ArpitChugh <arpitchugh.jbd.ac@gmail.com>
2022-05-25 16:32:39 +0530

    Tried a different approch to find sum of max subarray which does not work

--- LeetCode/cpp/MaxSubarray.cpp
+++ LeetCode/cpp/MaxSubarray.cpp
@@ -16,6 +16,37 @@ public:
         }
         return res;
     }
+    /* what I want to do is:
+       * continue to do increment left until next element is greater than the previous element and
+       * continue to decrement right until right most element is greater than one which comes next.
+       * after that add all the remaning elements in the vector.
+       *
+       * What is happening is:
+       * It does both increment/decrement initaily but after it finds the value which are greater it adds
+
+    */
+    int myMaxSubArray(vector<int>& nums) {
+            int left = 0;
+            int right = nums.size()-1;
+            int sum = 0;
+
+            for(int i =0; i < nums.size(); i++){
+                if (nums[left] < nums[left + 1]) {
+                    left += 1;
+                }
+                else if (nums[right] < nums[right - 1]) {
+                    right -= 1;
+                }
+                else {
+                    while (left<=right)
+                    {
+                        sum += nums[left] + nums[right];
+                    }
+                }
+            }
+            return sum;
+
+        }
 };
 
 int main() {
