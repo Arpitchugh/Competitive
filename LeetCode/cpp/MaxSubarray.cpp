976401177d428343a17531a0dda23dd4159fe69f
ArpitChugh <arpitchugh.jbd.ac@gmail.com>
2022-05-25 16:11:51 +0530

    leetcode Q53 return max value from a contiguous subarray from a vector

--- LeetCode/cpp/MaxSubarray.cpp
+++ LeetCode/cpp/MaxSubarray.cpp
@@ -1,5 +1,9 @@
 // Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 // A subarray is a contiguous part of an array.
+// return max element which can be created from this contiguous array
+#include <iostream>
+#include <vector>
+using namespace std;
 
 class Solution {
 public:
@@ -13,3 +17,10 @@ public:
         return res;
     }
 };
+
+int main() {
+    vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
+    Solution sol;
+    cout << sol.maxSubArray(nums);
+    return 0;
+}
