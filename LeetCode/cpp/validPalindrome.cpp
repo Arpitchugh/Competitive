6e0fc4b2ce465187c979d115cae2ff465de194df
ArpitChugh <arpitchugh.jbd.ac@gmail.com>
2022-05-25 15:55:32 +0530

    ascii value numbering added for future refrence

--- LeetCode/cpp/validPalindrome.cpp
+++ LeetCode/cpp/validPalindrome.cpp
@@ -4,31 +4,16 @@ using namespace std;
 class solution {
 public:
     bool isPalindrome(string s) {
-//         transform(s.begin(), s.end(), s.begin(), ::tolower);
-
-//         int l = 0;
-//         int r = s.length();
-
-//         while(l!=r or l<r){
-//             if(s[l] == s[r]) return true;
-//             else if(65 >= char(s[l]) >= 90 or 97 >= char(s[l]) >= 122){
-//                 l++;
-//             }
-//             else if(65 >= char(s[r]) >= 90 or 97 >= char(s[r]) >= 122){
-//                 r--;
-//             }
-//             else return false;
-//         }
-//         return true;
-//     }
     string str;
     for(int i=0;i<s.length();i++){
-
+        //Capital letters
         if(s[i]>=65 && s[i]<=90){
             s[i]=s[i]+32;
             str.push_back(s[i]);
         }
+        //Small letters
         else if(s[i]>=97 && s[i]<=122) str.push_back(s[i]);
+        //Numbers
         else if(s[i]>=48 && s[i]<=57) str.push_back(s[i]);
 
     }
