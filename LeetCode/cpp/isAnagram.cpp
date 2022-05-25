0aab504ac39f851cb359c7ae76e9813fd2cd2bb8
ArpitChugh <arpitchugh.jbd.ac@gmail.com>
2022-05-25 15:45:38 +0530

    Leetcode easy Q125 using two pointer finds if string is palindrome or not.

--- LeetCode/cpp/isAnagram.cpp
+++ LeetCode/cpp/isAnagram.cpp
@@ -5,24 +5,28 @@ int main(int argc, char const *argv[]) {
   string s = "anagarm";
   string t = "naagram";
   // logic
-  if(s.size() != t.size())
+  if(s.size() != t.size()){
         cout << false;
+  }
 
        unordered_map<char, int> sMap;
-
+       bool ans = true;
 
        for(int i=0; i<s.size(); i++) {
            sMap[s[i]]++;
+           // cout << sMap[s[i]]++ <<" ";
            sMap[t[i]]--;
+           // cout << sMap[t[i]]++ <<" ";
+           // cout << endl;
        }
 
        for(auto it=sMap.begin(); it!=sMap.end(); it++) {
            if(it->second != 0) {
-               cout << "False";
+               ans = false;
                break;
            }
        }
 
-       cout << "True";
+       cout << ans;
        return 0;
    }
