d6220ece2ad07a1b368e374547c1628916fdb6bf
ArpitChugh <arpitchugh.jbd.ac@gmail.com>
2022-06-01 23:41:48 +0530

    previous code does not work on some cases

--- LeetCode/cpp/medianOfTwoSortedArray.cpp
+++ LeetCode/cpp/medianOfTwoSortedArray.cpp
@@ -11,12 +11,12 @@ Output: 2.00000
 Explanation: merged array = [1,2,3] and median is 2.
 */
 
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-	double l = 0;
+	/* This does not work for nums1 = 1,2 and nums2 = 3,4
+  double l = 0;
 	double r;
 	// assigning r
 	if (nums1.size() < nums2.size()) { r = nums1.size() - 1; }
@@ -51,13 +51,62 @@ double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 		l = mid1 + 1;
 	}
 	return 0;
+  */
+	int n = nums1.size();
+	int m = nums2.size();
+	int cnt = ((n + m) / 2);
+	int sm = (n + m);
+
+	int st = 0, end = n;
+	while (st <= end)
+	{
+		int mid = st + ((end - st) / 2);
+		int rem = cnt - mid;
+
+		if (rem > m)
+		{
+			st = mid + 1;
+			continue;
+		}
+		if (mid > cnt)
+		{
+			end = mid - 1;
+			continue;
+		}
+
+		int l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
+		if (mid > 0)
+			l1 = nums1[mid - 1];
+		if (rem > 0)
+			l2 = nums2[rem - 1];
+		if (mid < n)
+			r1 = nums1[mid];
+		if (rem < m)
+			r2 = nums2[rem];
+
+		if (l1 <= r2 && l2 <= r1)
+		{
+			double x = max(l1, l2);
+			double y = min(r1, r2);
+			if (sm % 2 == 0)
+				return ((x + y) / 2.0);
+			else
+				return y;
+		}
+		else if (l1 > r2)
+			end = mid - 1;
+		else if (l2 > r1)
+			st = mid + 1;
+	}
+	return 0;
+
 
 }
 
 
 int main(int argc, char const* argv[]) {
-	vector<int> s = { 1,3,5 };
-	vector<int> t = { 2,3 };
+	vector<int> s = { 1,2 };
+	vector<int> t = { 3,4 };
 	cout << findMedianSortedArrays(s, t);
 	return 0;
 }
