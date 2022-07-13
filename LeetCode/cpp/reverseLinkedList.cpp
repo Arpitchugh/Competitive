79eae62205e9950e3764c7d798c8688b598a33e3
Arpit Chugh <arpitchugh.jbd.ac@gmail.com>
2022-07-13 20:10:40 +0530

    Q.206 Reverse Linked List

---
+++ LeetCode/cpp/reverseLinkedList.cpp
@@ -0,0 +1,33 @@
+#include "iostream"
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+ListNode* reverseList(ListNode* head) {
+	ListNode* pre = NULL;
+    ListNode* curr = head;
+    ListNode* nex = head;
+    
+    while(curr){
+        
+        nex = nex->next;
+        curr->next = pre;
+        pre = curr;
+        curr = nex;
+        
+    }
+    
+    return pre;
+}
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
