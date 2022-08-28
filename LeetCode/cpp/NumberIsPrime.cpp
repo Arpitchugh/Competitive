aabd33325a26fec7123c5e253d2bf0baf0f9b5ad
Arpit Chugh <arpitchugh.jbd.ac@gmail.com>
2022-08-28 15:53:08 +0530

    now it prints the array of all prime numbers till the input

--- LeetCode/cpp/NumberIsPrime.cpp
+++ LeetCode/cpp/NumberIsPrime.cpp
@@ -6,21 +6,24 @@ Prime -> 0 1 2 3 5 7 11 ....
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n;
-    cin>>n;
-    if(n==1){
-        cout<<"Not Prime";
-    }
-    else{
+bool isPrime(int n){
+    if(n == 1) return false;
+    
+    else {
         for(int i=2;i<=sqrt(n);i++){
-            if(n%i==0){
-                cout<<"Not Prime";
-                break;
-            }
-            else{
-                cout<<"Prime";
-            }
+            if(n%i == 0) return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    cout << "Enter number of prime numbers you want to print: ";
+    int n;
+    cin >> n;
+    for(int i=2;i<=n;i++){
+        if(isPrime(i)){   
+         cout << i << " ";
         }
     }
     return 0;
