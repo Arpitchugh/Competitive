cd799cafba50a383a7024729125c07076f515bc1
Arpit Chugh <arpitchugh.jbd.ac@gmail.com>
2022-11-28 12:13:31 +0530

    reverseArray

--- random/easyJS/reverseList.js
+++ random/easyJS/reverseList.js
@@ -1,5 +1,21 @@
- /**
-  * reverse an array
-  * reverse 2D array
-  * reverse linkedList
-  */
+/**
+ * reverse an array
+ * reverse 2D array
+ * reverse linkedList
+ */
+
+// SECTION array
+arr = [1, 2, 3, 4, 5, 6, 7, 8];
+const reverseArray = arr => {
+	let leftPointer = 0;
+	let rightPointer = arr.length - 1;
+	while (leftPointer <= rightPointer) {
+		let temp = arr[leftPointer];
+		arr[leftPointer] = arr[rightPointer];
+		arr[rightPointer] = temp;
+		leftPointer++;
+		rightPointer--;
+	}
+};
+reverseArray(arr);
+console.log(arr);
